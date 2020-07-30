eb9d7f2a3b66c8db373b3e914ed15ea790c48615
Junior Fernandes <juniorbass8@gmail.com>
2020-07-29 23:08:45 -0300

    Criando o component Calculator

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,13 +1,47 @@
 <template>
-  
+    <div class="calculator">
+        <Display value="1000" />
+        <Button label="AC" triple/>
+        <Button label="/" operation/>
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" operation/>
+        <Button label="5" />
+        <Button label="4" />
+        <Button label="3" />
+        <Button label="-" operation/>
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" operation/>
+        <Button label="0" double/>
+        <Button label="." />
+        <Button label="=" operation/>
+    </div>
+
 </template>
 
 <script>
-export default {
+import Display from '../components/Display'
+import Button from '../components/Button'
 
+export default {
+    components: { Button, Display }
 }
 </script>
+.calculator {
+
+}
 
 <style>
+    .calculator {
+        height: 320px;
+        width: 235px;
+        border-radius: 5px;
 
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+    }
 </style>
